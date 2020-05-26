bcedf1add9770bfaa637db97391836e385ed3621
Matt Green <magreen118@gmail.com>
2020-05-26 14:23:41 +0000

    Done.

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,9 @@
 def square_array(array)
-  # your code here
+  square_array = []
+  count = 0
+  while array[count] do
+    square_array.push((array[count])**2)
+    count += 1  
+  end
+  return square_array
 end
